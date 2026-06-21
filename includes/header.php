e21b6cb61922b2ff97a5ef0a9d904c405eaffc09
dkl195 <22070006@vnu.edu.vn>
2026-06-21 19:21:43 +0700

    Preserve navbar search keyword

--- includes/header.php
+++ includes/header.php
@@ -34,7 +34,7 @@ $baseUrl = '/webfinal';
                 <svg class="search-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                     <circle cx="11" cy="11" r="8"/><path d="M21 21l-4.35-4.35"/>
                 </svg>
-                <input type="text" name="q" class="search-input" placeholder="Tìm phòng, lab, sân...">
+                <input type="text" name="q" class="search-input" value="<?= e($_GET['q'] ?? '') ?>" placeholder="Tìm phòng, lab, sân...">
             </div>
         </form>
         <?php endif; ?>
